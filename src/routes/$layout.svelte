35528bc6ebe1e22107e12c8b69b72178ebf7a25c
ulwlu <ulwlu@users.noreply.github.com>
2021-01-01 00:05:00 +0000

    deploy: 5b8f024f624fd54baa33af9a9f8de946ba678520

--- src/routes/$layout.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import GlobalStyle from "./globalstyle.svelte";
-</script>
-
-<GlobalStyle/>
-<main>
-  <slot />
-</main>
-
--- src/routes/store.ts
+++
@@ -1,8 +0,0 @@
-import { writable } from "./localStore";
-
-export const preferences = writable(
-	"preferences",
-	{ theme: "dark" },
-);
-// import { writable } from "svelte/store"
-// export const theme = writable('dark')
